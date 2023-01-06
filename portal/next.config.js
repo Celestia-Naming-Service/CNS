6c9cb287f9488bda988d83e777a56cd0980cf31e
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-01-07 00:07:19 +0530

    Update next.config.js

--- portal/next.config.js
+++ portal/next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async exportPathMap(defaultPathMap) {
+    return {
+      "/api/data": { page: "/api/data" },
+    };
+  },
+};
